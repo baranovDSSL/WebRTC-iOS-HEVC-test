6e01ad6a23c4402975d5d7787495f62fcca39afb
Баранов Алексей <a.baranov@dssl.ru>
2025-09-10 18:21:43 +0500

    Fix: Use correct module name WebRTC instead of WebRTCLib

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "WebRTCLib",
-            targets: ["WebRTCLib"]
+            name: "WebRTC",
+            targets: ["WebRTC"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "WebRTCLib",
-            url: "https://github.com/baranovDSSL/WebRTC-iOS-HEVC-test/releases/download/v1.0.4/WebRTC.xcframework.zip",
+            name: "WebRTC",
+            url: "https://github.com/baranovDSSL/WebRTC-iOS-HEVC-test/releases/download/v1.0.5/WebRTC.xcframework.zip",
             checksum: "eb7807ee8e1c6f184827a94ac02f0c2d22896ad9dd03e352e4570c51f46ec081"
         )
     ]
