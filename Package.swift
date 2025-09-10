fbe0789e012fcf68c2005dc2bdad6aa665d81293
Баранов Алексей <a.baranov@dssl.ru>
2025-09-10 17:51:09 +0500

    Fix package name consistency: all names now WebRTCLib

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "WebRTC-lib",
+    name: "WebRTCLib",
     platforms: [
         .iOS(.v13)
     ],
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTCLib",
-            url: "https://github.com/baranovDSSL/WebRTC-iOS-HEVC-test/releases/download/v1.0.3/WebRTC.xcframework.zip",
+            url: "https://github.com/baranovDSSL/WebRTC-iOS-HEVC-test/releases/download/v1.0.4/WebRTC.xcframework.zip",
             checksum: "eb7807ee8e1c6f184827a94ac02f0c2d22896ad9dd03e352e4570c51f46ec081"
         )
     ]
